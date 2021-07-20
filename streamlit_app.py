18b82d65f5ef2cc3ae7fd1c31246c0eb3a6783e5
sebastiandres <sebastiandres@gmail.com>
2021-07-20 19:28:57 -0400

    Added support for multiple functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ from numpy import * #sin, cos, tan, log, exp, etc.
 
 LANGUAGE_DICT = {"ESP": {
                          "parameters_title":'Parámetros',
+                         "functions_expander":'Funciones',
+                         "add_function":'Agregar función',
+                         "clean_functions":'Borrar funciones',
+                         "function_color":'Color',
+                         "graph_expander":"Propiedades del gráfico",
                          "equation": "Ecuación",
                          "title_text": "Título del gráfico",
                          "title_value": "Mi titulo",
@@ -26,6 +31,11 @@ Inspirado en [http://xkcdgraphs.com/](http://xkcdgraphs.com/)
                         },
                  "ENG": {
                          "parameters_title":'Parameters',
+                         "functions_expander":'Functions',
+                         "function_color":'Color',
+                         "clean_functions":'Delete functions',
+                         "add_function":'Add new function',
+                         "graph_expander":"Graph configuration",
                          "equation": "Equation examples",
                          "title_text": "Title",
                          "title_value": "My Custom Title",
@@ -54,7 +64,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-def xkcd_plot(f, title, xlabel, ylabel, xmin, xmax, Nx):
+def xkcd_plot(f_list, title, xlabel, ylabel, xmin, xmax, Nx):
 
     with plt.xkcd():
         # Create the figure
@@ -65,12 +75,19 @@ def xkcd_plot(f, title, xlabel, ylabel, xmin, xmax, Nx):
         ax.spines.top.set_color('none')
         # Eval the data
         x = np.linspace(xmin, xmax, num=Nx) + 1E-16 # So if we divide by x no errors
-        y = eval(f)
-        # Plot
-        ax.plot(x, y)
+        y_list = []
+        for f, c in f_list:
+            y = eval(f)
+            # Plot
+            ax.plot(x, y, color=c)
+            y_list.append(y)
+        y = np.array(y_list)
         # Set the limits for the plot
-        ymin = int(np.floor(y.min())-1)
-        ymax = int(np.ceil(y.max())+1)
+        if len(f_list)>0:
+            ymin = int(np.floor(y.min())-1)
+            ymax = int(np.ceil(y.max())+1)
+        else:
+            ymin, ymax = xmin, xmax
         ax.set_xlim([xmin, xmax])
         ax.set_ylim([ymin, ymax])
         # Put the x ticks
@@ -90,26 +107,40 @@ def xkcd_plot(f, title, xlabel, ylabel, xmin, xmax, Nx):
         return fig
 
 # The side bar
-print(LANGUAGE_DICT.keys())
 SELECTED_LANGUAGE = st.sidebar.selectbox('', list(LANGUAGE_DICT.keys()))
 SLANG_DICT = LANGUAGE_DICT[SELECTED_LANGUAGE]
 st.sidebar.subheader(SLANG_DICT["parameters_title"])
-f = st.sidebar.text_input(SLANG_DICT["equation"],"sin(5*x)/x")
-title = st.sidebar.text_input(SLANG_DICT["title_text"], SLANG_DICT["title_value"])
-xlabel = st.sidebar.text_input(SLANG_DICT["xlabel_text"], "x")
-ylabel = st.sidebar.text_input(SLANG_DICT["ylabel_text"], "y")
-xmin = st.sidebar.number_input(SLANG_DICT["xmin_text"], value=-5)
-xmax = st.sidebar.number_input(SLANG_DICT["xmax_text"], value=+5)
-Nx = 1000
-with st.sidebar.beta_expander(SLANG_DICT["examples"]):
-    st.code("abs((x+4)*x)")
-    st.code("exp(-x**2)")
+
+# Initialize the session states - f_list has functions and colors
+if 'f_list' not in st.session_state:
+	st.session_state['f_list'] = [
+                                  ("5*exp(-x**2)", "g"),
+                                  ("sin(5*x)/x", "b"),
+        ]
+
+with st.sidebar.beta_expander(SLANG_DICT["functions_expander"]):
+    f = st.text_input(SLANG_DICT["equation"],"sin(5*x)/x")
+    c = st.color_picker(SLANG_DICT["function_color"], "#0000FF")
+    col1, col2 = st.beta_columns(2)
+    if col1.button(SLANG_DICT["add_function"]):
+        st.session_state['f_list'].append( (f, c) )
+    if col2.button(SLANG_DICT["clean_functions"]):
+        st.session_state['f_list'] = []
+
+with st.sidebar.beta_expander(SLANG_DICT["graph_expander"]):
+    title = st.text_input(SLANG_DICT["title_text"], SLANG_DICT["title_value"])
+    xlabel = st.text_input(SLANG_DICT["xlabel_text"], "x")
+    ylabel = st.text_input(SLANG_DICT["ylabel_text"], "y")
+    xmin = st.number_input(SLANG_DICT["xmin_text"], value=-5)
+    xmax = st.number_input(SLANG_DICT["xmax_text"], value=+5)
 
 st.sidebar.markdown(SLANG_DICT["links_md"])
+
 # The main view
 try:
-    fig = xkcd_plot(f, title, xlabel, ylabel, xmin, xmax, Nx)
+    fig = xkcd_plot(st.session_state['f_list'], title, xlabel, ylabel, xmin, xmax, Nx=1000)
     st.pyplot(fig)
-except:
+except Exception as e:
     st.error(SLANG_DICT["error_warning"])
     st.warning(SLANG_DICT["error_advice"])
+    st.exception(e)

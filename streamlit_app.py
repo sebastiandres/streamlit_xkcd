68b1c01dd8eca34135126ebb33a2d539a0d25650
sebastiandres <sebastiandres@gmail.com>
2021-09-01 18:30:10 -0400

    No longer beta

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,17 +30,17 @@ SLANG_DICT = LANGUAGE_DICT[st.session_state['SLANG']]
 
 st.sidebar.subheader(translate("parameters_title"))
 
-with st.sidebar.beta_expander(translate("functions_expander")):
+with st.sidebar.expander(translate("functions_expander")):
     f = st.text_input(translate("equation"), "sin(5*x)/x")
     c = st.color_picker(translate("function_color"), "#0000FF")
-    col1, col2 = st.beta_columns(2)
+    col1, col2 = st.columns(2)
     if col1.button(translate("add_function")):
         st.session_state['f_list'].append( (f, c) )
     if col2.button(translate("clean_functions")):
         st.session_state['f_list'] = []
     st.write(translate("functions_link"))
 
-with st.sidebar.beta_expander(translate("graph_expander")):
+with st.sidebar.expander(translate("graph_expander")):
     title = st.text_input(translate("title_text"), translate("title_value"))
     xlabel = st.text_input(translate("xlabel_text"), "x")
     ylabel = st.text_input(translate("ylabel_text"), "y")
